e0fa57bba35d2390360aede510f0c970061636d3
MarkusSMadsbakken <113692526+MarkussMadsbakken@users.noreply.github.com>
2024-05-23 18:25:37 +0200

    init

---
+++ lib/poker_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule PokerWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use PokerWeb, :controller` and
+  `use PokerWeb, :live_view`.
+  """
+  use PokerWeb, :html
+
+  embed_templates "layouts/*"
+end
